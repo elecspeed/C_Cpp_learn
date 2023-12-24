1a184ec73cde304f0d59ae818e12450fd3031f40
杨智 <b23042114@njupt.edu.cn>
2023-12-24 13:21:22 +0800

    补充了strncmp函数

--- 字符函数和字符串函数/字符函数和字符串函数.c
+++ 字符函数和字符串函数/字符函数和字符串函数.c
@@ -174,7 +174,7 @@
 // 
 // 函数声明
 // char *strncpy( char *strDest, const char *strSource, size_t count );
-// 与strcpy相比多了一个count，表示在strcpy的基础上只操作源字符串的前count个字节（或者说字符）
+// 与strcpy相比多了一个count，表示在strcpy的基础上只拷贝源字符串的前count个字节（或者说字符）
 // 
 // 注意事项：源字符串长度小于count时，目的字符串多出的待拷贝部分用'\0'替换；其余同strcpy
 //
@@ -218,7 +218,7 @@
 // 
 // 函数声明
 // char *strncat( char *strDest, const char *strSource, size_t count );
-// 表示在strcat的基础上只操作源字符串的前count个字节
+// 表示在strcat的基础上只追加源字符串的前count个字节
 // 
 // 注意事项：源字符串的长度大于count，追加完会再补加一个'\0'；长度小于count，追加到'\0'就停止
 //           即追加部分的后面总有一个'\0'
@@ -235,31 +235,33 @@
 //}
 
 //手搓strncat
-char* my_strncat(char* dest, const char* sour, unsigned int count)
-{
-	assert(dest);
-	assert(sour);
-	char* ret = dest;
-
-	//我写的
-	while (*dest)	//找目的字符串的'\0'
-		dest++;
-	 
-	while (*dest++ = *sour++ && count--);	//追加sour字符串
-	return ret;
-
-	//标准答案
-	//while (*dest++)
-	//	;
-	//dest--;
-
-	//while (count--)
-	//	if ((*dest++ = *sour++) == 0)
-	//		return(ret);
-
-	//*front = '\0';
-	//return ret;
-}
+//char* my_strncat(char* dest, const char* sour, unsigned int count)
+//{
+//	assert(dest);
+//	assert(sour);
+//	char* ret = dest;
+//
+//	//我写的
+//	while (*dest)	//找到目的字符串的'\0'
+//		dest++;
+//	 
+//	while (*dest++ = *sour++ && --count);	//追加sour字符串直到遇到'\0'或达到count个字符
+//	if (count == 0)
+//		*dest = '\0';					//如果到count个字符还没有遇到'\0'，就补追一个'\0'
+//	return ret;
+//
+//	//标准答案
+//	//while (*dest++)
+//	//	;
+//	//dest--;
+//
+//	//while (count--)
+//	//	if ((*dest++ = *sour++) == 0)
+//	//		return(ret);
+//
+//	//*front = '\0';
+//	//return ret;
+//}
 
 
 
@@ -267,11 +269,102 @@ char* my_strncat(char* dest, const char* sour, unsigned int count)
 // 
 // 函数声明
 // int strncmp( const char *string1, const char *string2, size_t count );
-// 表示在strcmp的基础上，只操作两个字符串的前count个字节
+// 表示在strcmp的基础上，只比较两个字符串的前count个字节
+// 
+// 标准规定同strcmp
 //
 
 //手搓strncmp
-//int my_strncmp(const char* dest, const char* sour, unsigned int count)
+//int my_strncmp(const char* str1, const char* str2, unsigned int count)
 //{
+//	assert(str1);
+//	assert(str2);
 //
+//	//找到了7种情况
+//	//我写的
+//	//if (!count) return 0;		//小心极端情况！
+//
+//	//while (*str1 == *str2)
+//	//{
+//	//	if (*str1 == '\0' || --count == 0)
+//	//		return 0;
+//	//	str1++;
+//	//	str2++;
+//	//}
+//	//return (*str1 - *str2);
+//
+//	//标准答案
+//    //size_t x = 0;
+//    //if (!count)             //处理极端情况
+//    //    return 0;
+//
+//    ////以四个字节为单位处理
+//    //if (count >= 4)
+//    //{
+//    //    /* unroll by four */
+//    //    for (; x < count - 4; x += 4)
+//    //    {
+//    //        str1 += 4;
+//    //        str2 += 4;
+//
+//    //        if (*(str1 - 4) == 0 || *(str1 - 4) != *(str2 - 4))
+//    //        {
+//    //            return(*(unsigned char*)(str1 - 4) - *(unsigned char*)(str2 - 4));
+//    //        }
+//
+//    //        if (*(str1 - 3) == 0 || *(str1 - 3) != *(str2 - 3))
+//    //        {
+//    //            return(*(unsigned char*)(str1 - 3) - *(unsigned char*)(str2 - 3));
+//    //        }
+//
+//    //        if (*(str1 - 2) == 0 || *(str1 - 2) != *(str2 - 2))
+//    //        {
+//    //            return(*(unsigned char*)(str1 - 2) - *(unsigned char*)(str2 - 2));
+//    //        }
+//
+//    //        if (*(str1 - 1) == 0 || *(str1 - 1) != *(str2 - 1))
+//    //        {
+//    //            return(*(unsigned char*)(str1 - 1) - *(unsigned char*)(str2 - 1));
+//    //        }
+//    //    }
+//    //}
+//    ///* residual loop */
+//    //for (; x < count; x++)
+//    //{
+//    //    if (*str1 == 0 || *str1 != *str2)
+//    //    {
+//    //        return(*(unsigned char*)str1 - *(unsigned char*)str2);
+//    //    }
+//    //    str1 += 1;
+//    //    str2 += 1;
+//    //}
+//    //return 0;
+//
+//    //把标准答案简化了一下
+//    if (!count) return 0;       //处理极端情况
+//
+//    while (count--)
+//    {
+//        if (*str1 == 0 || *str1 != *str2)
+//        {
+//            return (*(unsigned char*)str1 - *(unsigned char*)str2);
+//        }
+//        str1++;
+//        str2++;
+//    }
+//    return 0;
+//    //简化版的标准答案和我写的是两种不同的实现逻辑（画流程图方便观察）
+//    //我的是：比较的字符相等则进入循环接着比较，否则退出循环返回差值。
+//    //        进入循环后，检查count和刚才比较的字符，
+//    //        只要count==0或刚才比较的字符已经是'\0'了，就表明比较结束，返回0。
+//    //        否则比较下一对字符
+//    //简化版：只要比较次数没到count，就进入循环接着比，否则返回0。
+//    //        进入循环后，检查比较的字符是否相等且不为'\0'，
+//    //        相等且不为'\0'就接着比，否则返回差值
+//    //        （注意‘*str1 == 0 || *str1 != *str2’的补集为‘*str1 != 0 && *str1 == *str2’）
+//    // 
+//    // 两种逻辑均能满足找到的7种情况
+//    // 但实现原理不同，遇到新的情况时得到的结果就可能不同
+//    // 没有孰优孰劣，只看哪个能适应新情况
+//    //
 //}
