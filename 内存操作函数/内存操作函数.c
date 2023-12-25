a83378d3682c29e0a8a2f056a257702f803cde20
杨智 <b23042114@njupt.edu.cn>
2023-12-25 18:10:55 +0800

    学完部分“字符函数和字符串函数”，接着两个内存拷贝函数

---
+++ 内存操作函数/内存操作函数.c
@@ -0,0 +1,142 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <memory.h>
+#include <assert.h>
+
+//虽然有了字符函数和字符串函数，但这些函数只能操作字符（只有一个字节）和字符串（只有一个字节是0)
+// 对于整型数组，浮点型数组，结构体数组等类型不适用
+//
+//#include <string.h>
+//
+//int main()
+//{
+//    int arr1[] = { 1,2,3,4,5 };
+//    int arr2[10] = { 0 };
+//
+//    //char* dest, const char* sour
+//    strcpy(arr2, arr1);
+//
+//    //调试看结果
+//    return 0;
+//}
+//
+//因此引出内存操作函数（需要memory库或string库）
+
+
+
+//1.memcpy
+// 将strncpy声明中的char*改为void*就是memcpy的声明
+// 
+// 声明
+// void *memcpy( void *dest, const void *src, size_t count );   //count单位是字节
+// 
+//struct S
+//{
+//    char name[10];
+//    int age;
+//};
+//int main()
+//{
+//    int arr1[] = { 1,2,3,4,5 };
+//    int arr2[10] = { 0 };
+//    memcpy(arr2, arr1, sizeof(arr1));
+//
+//    struct S arr3[] = { {"peter", 18}, {"mike", 20} };
+//    struct S arr4[3] = { 0 };
+//    memcpy(arr4, arr3, sizeof(arr3));
+//
+//    //调试看结果
+//    return 0;
+//}
+
+//注意事项：拷贝的双方（拷贝源与拷贝目的）不能重叠
+// C标准规定：
+// memcpy至少可以完成不重叠拷贝
+// memmove则是不管重不重叠都可以完成
+// 
+// 因为vs编译环境下memcpy等同于memmove，所以不作示例
+//
+
+//手搓memcpy（仅仅满足标准）
+//void* my_memcpy(void* dest, const void* sour, unsigned int count)
+//{
+//    assert(dest);
+//    assert(sour);
+//
+//    //我写的
+//    if (!count) return dest;
+//    
+//    char* d = (char*)dest;
+//    char* s = (char*)sour;
+//    while (*d++ = *s++, --count);
+//    
+//    return dest;
+//
+//    //鹏哥答案
+//    //void* ret = dest;
+//    //while (count--)
+//    //{
+//    //    *(char*)dest = *(char*)sour;
+//    //    ++(char*)dest;
+//    //    ++(char*)sour;
+//    //}
+//    //return ret;
+//}
+
+
+
+//2.memmove
+// 
+// 声明
+// void *memmove( void *dest, const void *src, size_t count );
+// 和memcpy的声明一模一样（有深意）
+// 
+// 注意事项：搞清楚从哪里拷贝到哪里，容易乱
+//
+
+//手搓memmove
+void* my_memmove(void* dest, const void* sour, size_t count)
+{
+    assert(dest);
+    assert(sour);
+    char* d = (char*)dest;
+    char* s = (char*)sour;
+
+    if (d < s)
+        while (*d++ = *s++, --count);   //dest在sour左边，则从左到右逐个拷贝
+    else if (d == s)
+        return d;                       //dest == sour，完全重叠，不用拷贝
+    else if (d <= s + count)
+    {
+        s += count - 1;
+        d += count - 1;
+        while (*d-- = *s--, --count);   //dest在(sour, sour+count]中间，则从右到左逐个拷贝
+    }
+    else
+    {
+        //dest在sour+count右边，可以从左到右逐个拷贝
+        //while (*d++ = *s++, --count);
+
+        //也可以从右到左逐个拷贝
+        s += count - 1;
+        d += count - 1;
+        while (*d-- = *s--, --count);
+    }
+    //可以画区间图方便理解
+
+    return dest;
+}
+int main()
+{
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+    //my_memmove(arr, arr + 2, 5 * sizeof(arr[0]));
+    my_memmove(arr + 3, arr + 1, 20);
+
+    int i = 0;
+    for (i = 0; i < 10; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
