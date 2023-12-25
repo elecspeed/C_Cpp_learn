3bdb5b75f189497fb3b368e9404e74a94035a3cd
杨智 <b23042114@njupt.edu.cn>
2023-12-25 12:58:45 +0800

    继续学习字符函数和字符串函数

--- 字符函数和字符串函数（下）/字符函数和字符串函数（下）.c
+++ 字符函数和字符串函数（下）/字符函数和字符串函数（下）.c
@@ -9,7 +9,7 @@
 //
 //int main()
 //{
-//	char arr1[] = "abcdefabcdef";
+//	char arr1[] = "abcdddefabcdef";
 //	char arr2[] = "def";
 //	char* ret = strstr(arr1, arr2);
 //	if (ret == NULL)
@@ -25,39 +25,155 @@
 // 
 
 //手搓strstr
-char* my_strstr(const char* str1, const char* str2)
-{
-	assert(str1);
-	assert(str2);
-	if (*str2 == '\0') return str1;
+//char* my_strstr(const char* str1, const char* str2)
+//{
+//	assert(str1);
+//	assert(str2);
+//	if (*str2 == '\0') return (char*)str1;
+//
+//	//法一：用三个指针变量，两个往后探测，一个用来标记（就是照着标准答案来写的，我写的太low了）
+//	//char* p1 = NULL;
+//	//char* p2 = NULL;			//p1和p2临时指针
+//	//char* curr = (char*)str1;			//当前指针，标记当前走到哪里
+//
+//	//while (*curr)				//curr会一直往后走直到'\0'
+//	//{
+//	//	//校准
+//	//	p1 = curr;
+//	//	p2 = (char*)str2;
+//
+//	//	//用临时指针往后探测
+//	//	while (*p1 && *p2 && *p1 == *p2)
+//	//	{
+//	//		p1++;
+//	//		p2++;
+//	//	}
+//	//	//探测完毕
+//
+//	//	//p2遇到'\0'说明子串存在，返回当前指针
+//	//	if (!*p2) return curr;
+//
+//	//	//p2没遇到'\0'而p1遇到，说明curr往后的子串长度小于str2的，再探测也没有意义，返回NULL
+//	//	if (!*p1) return NULL;
+//
+//	//	//p1和p2都没遇到'\0'，进行新一轮探测
+//	//	curr++;
+//
+//	//	//那如果p1和p2都遇到'\0'呢？
+//	//	//子串存在，返回当前指针
+//	//	//注意上面三种情况已经包含全部情况：
+//	//	// p1遇到'\0'（不管p2遇到没）；
+//	//	// p1没遇到p2遇到；
+//	//	// p1p2都没遇到
+//	//}
+//	//return NULL;
+//
+//	//法二：kmp算法（字符串匹配算法）（略）
+//}
 
-	//法一：用三个指针变量，两个往后探测，一个用来标记
-	char* p1 = NULL;
-	char* p2 = NULL;			//p1和p2临时指针
-	char* curr = str1;			//当前指针，标记当前走到哪里
 
-	while (*curr)				//curr会一直往后走直到'\0'
-	{
-		//校准
-		p1 = curr;
-		p2 = str2;
 
-		//用临时指针往后探测
-		while (*p1 && *p2 && *p1 == *p2)
-		{
-			p1++;
-			p2++;
-		}
-		//探测完毕
+//9.strtok
+// 用来分割字符串（遇到标志符就把其改为'\0'）
+// 
+// 声明
+// char *strtok( char *strToken, const char *strDelimit );
+// 
+// 用法：
+// 1.第一个参数传参时，传的是要被分割的str（或NULL）
+// 2.第二个参数传参时，传的是标志符的集合（也是字符串）
+// 3.第一个参数不是NULL：函数会把传进来的str的第一个标记改为'\0'，并保存该标记的下一个字符的地址
+// 4.第一个参数是NULL：函数将在保存的地址往后继续查找。
+// 5.没有遇到标志符，返回NULL
+//   遇到了，返回分割出去的一部分字符串的首地址
+//
+//int main()
+//{
+//	char arr[] = "192.168.0.100";
+//	//char p[] = {'.'};
+//	char p[] = ".";			//注意二者区别。"."有两个标志符，'.'和'\0'
+//
+//	char buf[1024] = { 0 };
+//	strcpy(buf, arr);		//直接对arr使用strtok会破坏数据。使用strtok前要拷贝一下
+//
+//	//char* ret = strtok(buf, p);
+//	//printf("%s\n", ret);
+//	//
+//	//int i = 0;
+//	//for (i = 0; i < 3; i++)
+//	//{
+//	//	ret = strtok(NULL, p);
+//	//	printf("%s\n", ret);
+//	//}
+//
+//	//将上面的代码改写一下
+//	char* ret = NULL;
+//	for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL, p))
+//	{
+//		printf("%s\n", ret);
+//	}
+//	//妙用for循环的机制
+//
+//	return 0;
+//}
+// 
+// strtok函数内部可以记住地址，推测是用静态变量实现的
+//
 
-		//p2遇到'\0'说明子串存在，返回当前指针
-		if (!*p2) return curr;
+//手搓strtok（略）
 
-		//p2没遇到'\0'而p1遇到，说明curr往后的子串长度小于str2的，再探测也没有意义，返回NULL
-		if (!*p1) return NULL;
 
-		//p1和p2都没遇到'\0'，进行新一轮探测
-		curr++;
-	}
-	return NULL;
-}
+
+//10.strerror和_strerror
+// Get a system error message (strerror) or prints a user-supplied error message (_strerror).
+// 
+// 声明
+// char *strerror( int errnum );
+// char *_strerror( const char *strErrMsg );（略）
+// 
+// 返回错误码errnum对应的错误信息（把错误码转换成能理解的错误信息，然后返回）
+//
+//int main()
+//{
+//	int i = 0;
+//	char* str = NULL;
+//	for (i = 0; i < 46; i++)
+//	{
+//		str = strerror(i);
+//		printf("%s\n", str);
+//	}
+//	return 0;
+//}
+// 
+//错误码  错误信息
+// 0  -   No error
+// 1  -   Operation not permitted
+// 2  -   No such file or directory
+// ...
+
+//实际用法
+//#include <errno.h>
+//int main()
+//{
+//	//char* str = strerror(errno);
+//	//printf("%s\n", str);
+//	//errno是一个全局变量，用来存错误码
+//	//C库函数在执行过程中，发生了错误，就会把对应的错误码，赋值给errno
+//
+//	//打开文件test.txt
+//	FILE* pf = fopen("test.txt", "r");
+//
+//	//FILE：定义好的结构体。
+//	//fopen:打开文件函数。
+//	//"r"：表示以只读的方式打开
+//	// 
+//	//意为在本目录下以只读的方式打开文件test.txt，pf接收函数返回值
+//	//详见"文件操作"
+//
+//
+//	if (pf == NULL)
+//		printf("%s\n", strerror(errno));
+//	else
+//		printf("open file success\n");
+//	return 0;
+//}
