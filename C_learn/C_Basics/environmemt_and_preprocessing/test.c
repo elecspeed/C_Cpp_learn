a90b04e2d444c3f67c395b2653d359dc1cec9e1a
杨智 <b23042114@njupt.edu.cn>
2024-03-23 23:28:47 +0800

    学习 C 预处理

--- C_learn/C_Basics/environmemt_and_preprocessing/test.c
+++ C_learn/C_Basics/environmemt_and_preprocessing/test.c
@@ -125,14 +125,15 @@
 //    printf("%s\n", __FUNCDNAME__);
 //    printf("%s\n", __FUNCSIG__);
 //    printf("%s\n", __FUNCTION__);
-//    // 上面的预定义符号可用来写日志文件
 //
+//    //printf("%d\n", __STDC__);
 //    // 特殊的预定义符号 __STDC__
 //    // 如果编译器严格遵循 C 标准，其值为 1，否则未定义
-//    //printf("%d\n", __STDC__);
 //
 //    return 0;
 //}
+// 预定义符号的应用场景：日志文件
+//
 
 // 预处理指令。以 # 开头
 // #define
@@ -142,27 +143,139 @@
 // #if
 // #endif
 // #ifdef
-// #line
+// 等等。
 //
 
 // #define 定义标识符
 //
 
-#define TEN 10
-#define STR "hello"
-#define reg register
+//#define TEN 10
+//#define STR "hello"
+//#define reg register
+//
+//#define do_forever for(;;)
+//#define DEBUG_PRINT printf("file:%s\tline:%d\t"\
+//                        "date:%s\ttime:%s\n",\
+//                        __FILE__, __LINE__,\
+//                        __DATE__, __TIME__)
+//int main()
+//{
+//    printf("%d\n", TEN);
+//    printf("%s\n", STR);
+//    reg int a;
+//
+//    DEBUG_PRINT;
+//    return 0;
+//}
+// #define 定义的后面一般都不加分号
+//
 
-#define do_forever for(;;)
-#define DEBUG_PRINT printf("file:%s\tline:%d\t"\
-                        "date:%s\ttime:%s\n",\
-                        __FILE__, __LINE__,\
-                        __DATE__, __TIME__)
-int main()
-{
-    printf("%d\n", TEN);
-    printf("%s\n", STR);
-    reg int a;
+// #define 定义宏
+//
 
-    DEBUG_PRINT;
-    return 0;
-}
+//#define FIVE 5
+//#define TEN 10
+//#define MAX(a, b) ((a)>(b)?(a):(b))
+//int main()
+//{
+//    int a = 22, b = 11;
+//    printf("%d\n", MAX(a, b));
+//    printf("%d\n", MAX(FIVE, TEN));
+//    return 0;
+//}
+// #define 定义宏或标识符，一般都大写。
+// 定义宏时，宏名和左边的小括号之间不能有空白；调用宏时可以。
+// 定义宏时，不要吝啬小括号。
+// 调用宏时，先检查宏参数中有没有 #define 定义的符号，有就替换；然后才是宏替换。
+// 宏不能递归。
+//
+
+// # 和 ##
+//
+
+// # 的应用场景：把宏参数插入字符串中
+// （函数不能实现该功能）
+// 
+//#define PRINT(X) printf("the value of "#X" is %d\n", (X))
+//int main()
+//{
+//    int a = 10, b = 20;
+//    //printf("the value of a is %d\n", a);
+//    PRINT(a);
+//    PRINT(b);
+//    return 0;
+//}
+// 宏定义中，#宏参数 == "宏参数名"，即 #X == "X"。
+// 然后进行宏替换。
+//
+
+// ##：把宏参数拼接成新符号，然后宏替换。
+//
+
+//#define CAT(a, b) a##b
+//int main()
+//{
+//    int tmp1024 = 100;
+//    printf("%d\n", CAT(tmp, 1024));
+//    return 0;
+//}
+
+// 带副作用的宏参数
+// a++, a--, ++a, --a
+// 当使用带副作用的宏参数时，要查看宏定义
+// 有些宏参数在宏定义中多次求值或多次出现，如果带副作用，会导致不可预料的后果
+//
+
+// 宏和函数的比较
+//
+
+//#define MAX(X, Y) ((X)>(Y)?(X):(Y))
+//double max_double(double x, double y) { return x > y ? x : y; }
+//int max_int(int x, int y) { return x > y ? x : y; }
+//int main()
+//{
+//    int a = 10, b = 20;
+//    double c = 3.14, d = 3.1415;
+//
+//    printf("max_double(c, d) = %f\n", max_double(c, d));
+//    printf("MAX(c, d) = %f\n\n", MAX(c, d));
+//    printf("max_int(a, b) = %d\n", max_int(a, b));
+//    printf("MAX(a, b) = %d\n", MAX(a, b));
+//    return 0;
+//}
+
+/*
+ * 1.函数调用时有系统开销，宏调用没有。
+ * 2.函数调用是调用同一个地方的同一个代码，宏调用是宏替换。
+ * 3.函数形参要声明类型，宏参数则无关类型，甚至参数可以是类型。
+ * 4.函数可以调试，宏不行。
+ * 5.函数可以递归，宏不行。
+ * 6.函数实参可以自增自减，宏不行。
+ * 7.宏可以 #，##，函数不行。
+ */
+
+//#define MALLOC(num, type)\
+//        (type *)malloc (num * sizeof(type))
+//int main()
+//{
+//    int* p = MALLOC(10, int);
+//    free(p);
+//    p = NULL;
+//    return 0;
+
+// 如果已有的一个名字需要重新定义，那么旧的定义首先要移除。
+// #undef：移除一个宏定义
+//
+
+//#define TEN 10
+//int main()
+//{
+//    printf("%d\n", TEN);
+//#undef TEN
+//    //printf("%d\n", TEN);
+//#define TEN 11
+//    printf("%d\n", TEN);
+//
+//    return 0;
+//}
+
