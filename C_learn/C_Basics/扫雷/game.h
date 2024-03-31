6a9c1c7c979adc1f2eb6c69ae7a8fef5717ab103
杨智 <b23042114@njupt.edu.cn>
2024-03-31 15:22:33 +0800

    复习 C 基础

--- C_learn/C_Basics/扫雷/game.h
+++ C_learn/C_Basics/扫雷/game.h
@@ -8,22 +8,19 @@
 // 虚拟的行和列
 #define ROW_VIRTUAL 9
 #define COL_VIRTUAL 9
-// 真实的行和列
+// 实际的行和列
 #define ROW_REAL ROW_VIRTUAL + 2
 #define COL_REAL COL_VIRTUAL + 2
 // 雷的数量
 #define MINE 10
 
-// 剩下的格子数，用来判断是否胜利
-extern int remaining_blank;
-
 void InitBoard(char board[][COL_REAL], int row, int col, char symbol);
 
 void DisplayBoard(char board[][COL_REAL], int row, int col);
 
-void MakeMine(char board[][COL_REAL], int row, int col);
+void SetMine(char board[][COL_REAL], int row, int col, int mine);
 
-int ClearMine(char front_board[][COL_REAL], char back_board[][COL_REAL],
-              int row, int col);
+void ClearMine(char front_board[][COL_REAL], char back_board[][COL_REAL],
+               int row, int col);
 
 #endif
--- C_learn/C_Basics/扫雷/geme.c
+++ C_learn/C_Basics/扫雷/geme.c
@@ -1,101 +1,119 @@
 #include "game.h"
 
 // 剩下的格子数，用来判断是否胜利
-int remaining_blank = ROW_VIRTUAL * COL_VIRTUAL - MINE;
+static int remaining_blank = ROW_VIRTUAL * COL_VIRTUAL - MINE;
 
 static void
 CountMine(char front_board[][COL_REAL], char back_board[][COL_REAL],
-		  int row, int col, int x, int y)
+          int row, int col, int x, int y)
 {
-	// 递归终止
-	if (x < 1 || x > row || y < 1 || y > col ||
-		back_board[x][y] != '.')
-		return;
+    // 递归终止
+    if (x < 1 || x > row - 2 || y < 1 || y > col - 2 ||
+        back_board[x][y] != '.')
+        return;
 
-	// 数周围有几颗雷
-	int count = 0;
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < 3; ++j)
-			if (back_board[x - 1 + i][y - 1 + j] == '#')
-				++count;
-	--remaining_blank;
-	if (count == 0)
-	{
-		front_board[x][y] = '-';
-		back_board[x][y] = '-';
-		// 空格，递归接着数雷
-		// 左上角递归
-		CountMine(front_board, back_board,
-				  row, col, x - 1, y - 1);
-		// 右边递归
-		CountMine(front_board, back_board,
-				  row, col, x, y + 1);
-		// 下边递归
-		CountMine(front_board, back_board,
-				  row, col, x - 1, y);
-	}
-	else
-		front_board[x][y] = '0' + count;
+    // 数周围有几颗雷
+    int count = 0;
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (back_board[x - 1 + i][y - 1 + j] == '#')
+                ++count;
+    --remaining_blank;
+    if (count == 0)
+    {
+        front_board[x][y] = back_board[x][y] = ' ';
+        // 空格，递归接着数雷
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                CountMine(front_board, back_board,
+                          row, col, x - 1 + i, y - 1 + j);
+    }
+    else
+        back_board[x][y] = front_board[x][y] = '0' + count;
 }
 void InitBoard(char board[][COL_REAL], int row, int col, char symbol)
 {
-	// 用 symbol 初始化棋盘
-	for (int i = 0; i < row; ++i)
-		for (int j = 0; j < col; ++j)
-			board[i][j] = symbol;
+    // 用 symbol 初始化棋盘
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < col; ++j)
+            board[i][j] = symbol;
 }
 void DisplayBoard(char board[][COL_REAL], int row, int col)
 {
-	// 打印棋盘
-	putchar(' ');
-	for (int i = 1; i < row - 1; ++i)
-		printf(" %d", i);
-	printf(" x\n");
-	for (int i = 1; i < row - 1; ++i)
-	{
-		printf("%d", i);
-		for (int j = 1; j < col - 1; ++j)
-			printf(" %c", board[i][j]);
-		putchar('\n');
-	}
-	printf("y\n");
+    // 打印棋盘
+    putchar(' ');
+    for (int i = 1; i < row - 1; ++i)
+        printf(" %d", i);
+    printf(" x\n");
+    for (int i = 1; i < row - 1; ++i)
+    {
+        printf("%d", i);
+        for (int j = 1; j < col - 1; ++j)
+            printf(" %c", board[i][j]);
+        putchar('\n');
+    }
+    printf("y\n");
 }
-void MakeMine(char board[][COL_REAL], int row, int col)
+void SetMine(char board[][COL_REAL], int row, int col, int mine)
 {
-	// 随机生雷
-	int random_x, random_y, mine;
-	random_x = random_y = mine = 0;
-	while (mine < MINE)
-	{
-		random_x = rand() % 9 + 1;
-		random_y = rand() % 9 + 1;
-		if (board[random_x][random_y] != '#')
-		{
-			board[random_x][random_y] = '#';
-			++mine;
-		}
-	}
+    // 随机生雷
+    int random_x, random_y;
+    random_x = random_y = 0;
+    while (mine > 0)
+    {
+        random_x = rand() % (row - 2) + 1;
+        random_y = rand() % (col - 2) + 1;
+        if (board[random_x][random_y] != '#')
+        {
+            board[random_x][random_y] = '#';
+            --mine;
+        }
+    }
 }
-int ClearMine(char front_board[][COL_REAL], char back_board[][COL_REAL],
-			  int row, int col)
+void ClearMine(char front_board[][COL_REAL], char back_board[][COL_REAL],
+               int row, int col)
 {
-	// 玩家排雷。如果踩雷返回 1，否则返回 0
-	int x, y;
-	x = y = 0;
-	printf("Enter the index like x,y :");
-	scanf("%d,%d", &y, &x);
-
-	if (x < 1 || x > ROW_VIRTUAL ||
-		y < 1 || y > COL_VIRTUAL)
-		/* 处理非法输入 */
-		printf("Illegal index.Please enter again.\n");
-	else if (back_board[x][y] == '#')
-		/* 踩雷了 */
-		return 1;
-	else
-	{
-		// 数周围有几颗雷
-		CountMine(front_board, back_board,
-				  row, col, x, y);
-	}
+    // 玩家排雷。
+    printf("There are %d mines in total.\n", MINE);
+    int x, y;
+    x = y = 0;
+    while (remaining_blank > 0)
+    {
+        // 游戏的核心部分：游戏过程、判断输赢
+        // 玩家输入
+        printf("Enter the index like x,y :");
+        scanf("%d,%d", &y, &x);
+        while (x < 1 || x > row - 2 || y < 1 || y > col - 2 ||
+               front_board[x][y] != '*')
+        {
+            /* 处理非法输入 */
+            printf("-----------------------------------------\n"
+                   "Illegal index.\n"
+                   "Please enter again.\n");
+            printf("Enter the index like x,y :");
+            scanf("%d,%d", &y, &x);
+        }
+        // 判断是否触雷
+        if (back_board[x][y] == '#')
+        {
+            // 踩雷了
+            // 先与用户交互，再把雷的分布展示出来
+            printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+                   "Oh,\n"
+                   "it seems that you triggered a mine.\n"
+                   "Don't be sad,\n"
+                   "you can try it again.\n");
+            DisplayBoard(back_board, ROW_REAL, COL_REAL);
+            return;
+        }
+        else
+        {
+            // 没踩雷
+            // 数周围有几颗雷
+            CountMine(front_board, back_board, row, col, x, y);
+            DisplayBoard(front_board, ROW_REAL, COL_REAL);
+        }
+    }
+    printf("*****************************************\n"
+           "          Congratulations!\n");
 }
