9965871d01cb636f0c69aeab20f069d87df93e3d
杨智 <b23042114@njupt.edu.cn>
2024-07-26 23:14:02 +0800

    迭代器

---
+++ Cpp_learn/improvement_to_C/iterator/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+ * 迭代器（iterator，又译为泛型指针）
+ * 迭代器对于容器，好比指针对于数组。
+ * 换句话说，
+ * 迭代器是模仿指针和数组的关系，
+ * 在容器的基础上抽象出的概念。
+ */
+int main()
+{
+    return 0;
+}
